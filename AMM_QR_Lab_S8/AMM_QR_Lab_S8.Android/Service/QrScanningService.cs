4c058fb8e7cf9f7e6eb19bcb35e7cac0603d87d8
Dario Turin <dturin1997@gmail.com>
2022-10-11 17:41:27 -0500

    Agregar archivos de proyecto.

---
+++ AMM_QR_Lab_S8/AMM_QR_Lab_S8.Android/Service/QrScanningService.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Threading.Tasks;
+using ZXing.Mobile;
+using Xamarin.Forms;
+using AMM_QR_Lab_S8.Service;
+using AMM_QR_Lab_S8.Droid.Service;
+using Android.Nfc.Tech;
+
+[assembly: Dependency(typeof(QrScanningService))]
+
+namespace AMM_QR_Lab_S8.Droid.Service
+{
+    public class QrScanningService : IQrScanningService
+    {
+        public async Task<string> ScanAsync()
+        {
+            MobileBarcodeScanner.Initialize(MainActivity.Instance.Application);
+
+            var optionsDefault = new MobileBarcodeScanningOptions();
+            var optionsCustom = new MobileBarcodeScanningOptions();
+
+            var scanner = new MobileBarcodeScanner()
+            {
+                TopText = "Scan the QR Code",
+                BottomText = "Please Wait",
+            };
+
+            var scanResult = await scanner.Scan(optionsCustom);
+            return scanResult.Text;
+        }
+    }
+}
---
+++ AMM_QR_Lab_S8/AMM_QR_Lab_S8/Service/IQrScanningService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMM_QR_Lab_S8.Service
+{
+    public interface IQrScanningService
+    {
+        Task<string> ScanAsync();
+
+    }
+}
